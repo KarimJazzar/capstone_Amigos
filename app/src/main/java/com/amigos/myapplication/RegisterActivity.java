1cb1044c5805870c3c3a07f798d0d8434d30b258
Karim El Jazzar <jazzar.karim@gmail.com>
2022-03-17 01:31:24 +0200

    Added user info documents in firebase after registration for profile editing later

--- app/src/main/java/com/amigos/myapplication/RegisterActivity.java
+++ app/src/main/java/com/amigos/myapplication/RegisterActivity.java
@@ -11,10 +11,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -24,6 +32,9 @@ public class RegisterActivity extends AppCompatActivity {
     Button btnRegister;
 
     FirebaseAuth mAuth;
+    //FirebaseDatabase database = FirebaseDatabase.getInstance();
+    //DatabaseReference documentReference;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +50,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         btnRegister.setOnClickListener(view ->{
             createUser();
+
         });
 
         tvLoginHere.setOnClickListener(view ->{
@@ -61,6 +73,25 @@ public class RegisterActivity extends AppCompatActivity {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
+                        String currentuid = task.getResult().getUser().getUid();
+                        Map<String,String> mail = new HashMap<>();
+                        mail.put("email", email);
+
+                        db.collection("User Info").document(currentuid)
+                                .set(mail)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        //Log.d(TAG, "DocumentSnapshot successfully written!");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        //Log.w(TAG, "Error writing document", e);
+                                    }
+                                });
+
                         Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                     }else{
