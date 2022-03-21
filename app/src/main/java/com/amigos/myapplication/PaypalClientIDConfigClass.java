a5230bba83e91c1b0b3b3ee601d7f70aae40a68e
Simranjit Singh <C0809865@mylambton.ca>
2022-03-21 13:15:46 -0400

    Added Paypal ClientID class

---
+++ app/src/main/java/com/amigos/myapplication/PaypalClientIDConfigClass.java
@@ -0,0 +1,6 @@
+package com.amigos.myapplication;
+
+public class PaypalClientIDConfigClass {
+    //PayPal Sandbox ClientID
+    public static final String PAYPAL_CLIENT_ID = "ARIvFFcMTmMlKuDeFLXSp7cpT8OVAZWc6_As4cS7lT9TAkjOBG22rDa7IAp7MlDCnNenJrskyS01-NbM";
+}
