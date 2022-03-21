2df6e6c380dea8c3be140bbe6f6d8fc3866ed337
Simranjit Singh <C0809865@mylambton.ca>
2022-03-21 13:58:57 -0400

    Implemented PayPal Payment Service Using REST API

--- app/src/main/java/com/amigos/myapplication/BookingActivity.java
+++ app/src/main/java/com/amigos/myapplication/BookingActivity.java
@@ -1,14 +1,103 @@
 package com.amigos.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+
+import java.math.BigDecimal;
 
 public class BookingActivity extends AppCompatActivity {
+
+    private Button btnBook;
+    private int PAYPAL_REQ_CODE = 12;
+    private static PayPalConfiguration paypalConfig = new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+            .clientId(PaypalClientIDConfigClass.PAYPAL_CLIENT_ID);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+
+        btnBook = findViewById(R.id.btnBook);
+
+        Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, paypalConfig);
+        startService(intent);
+
+
+        btnBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PaypalpaymentsMethod();
+            }
+        });
     }
+
+    private void PaypalpaymentsMethod() {
+        PayPalPayment payment = new PayPalPayment(new BigDecimal(50), "CAD"
+                , "Test Payment", PayPalPayment.PAYMENT_INTENT_SALE);
+
+        Intent intent = new Intent(this, PaymentActivity.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, paypalConfig);
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+
+        startActivityForResult(intent, PAYPAL_REQ_CODE);
+        //someActivityResultLauncher.launch(intent);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PAYPAL_REQ_CODE){
+
+            if(resultCode == Activity.RESULT_OK){
+                Toast.makeText(this, "Payment Made Successfully!", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Payment Is Unsuccessful", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
+
+
+    //Receiver for intent from next activity to get the payment status and change it in current activity
+    /*ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    int status = 1;
+                    if (status == 1){
+
+                        if (status == Activity.RESULT_OK){
+                            Toast.makeText(MainActivity.this, "Payment Made Successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Payment Is Unsuccessful", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }
+            });*/
+
 }
