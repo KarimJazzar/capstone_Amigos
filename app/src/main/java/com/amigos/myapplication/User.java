c7bfc3fa22215ab70599cf7d3f3db3bb06d8b3d8
Daniel Miolan <miolan96@gmail.com>
2022-03-23 19:54:36 -0400

    Create user class.

---
+++ app/src/main/java/com/amigos/myapplication/User.java
@@ -0,0 +1,49 @@
+package com.amigos.myapplication;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String phoneNuber;
+    private String email;
+    private String profilePicture;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNuber() {
+        return phoneNuber;
+    }
+
+    public void setPhoneNuber(String phoneNuber) {
+        this.phoneNuber = phoneNuber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+}
