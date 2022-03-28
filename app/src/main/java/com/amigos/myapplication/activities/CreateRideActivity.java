e1b502c9d02cc69f91d97adb0d2647e08db79f5b
febinrukfan772 <c0826772@mylambton.ca>
2022-03-27 23:39:31 -0400

    added polyline on start and end points

--- app/src/main/java/com/amigos/myapplication/activities/CreateRideActivity.java
+++ app/src/main/java/com/amigos/myapplication/activities/CreateRideActivity.java
@@ -12,7 +12,9 @@ import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -28,6 +30,14 @@ import com.amigos.myapplication.helpers.FirebaseHelper;
 import com.amigos.myapplication.helpers.UserHelper;
 import com.amigos.myapplication.models.Geopoint;
 import com.amigos.myapplication.models.User;
+import com.directions.route.AbstractRouting;
+import com.directions.route.Route;
+import com.directions.route.RouteException;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -35,26 +45,36 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.AutocompletePrediction;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.api.net.FetchPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class CreateRideActivity extends AppCompatActivity  implements OnMapReadyCallback{
+public class CreateRideActivity extends AppCompatActivity  implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener, RoutingListener {
 
     MapView mapView;
     private Button createButton;
     private Button backBtutton;
     private ImageView tripLessSeat,tripMoreSeat;
-    private EditText   inputPrice, inputSeats;
+    private EditText   inputPrice ;
+    private TextInputEditText inputSeats;
     private AutoCompleteTextView inputFrom,inputTo;
     private TextView inputDate,inputTime;
 
@@ -99,6 +119,7 @@ public class CreateRideActivity extends AppCompatActivity  implements OnMapReady
                 DateTimeHelper.selectDate(inputDate,CreateRideActivity.this);
             }
         });
+
 
 
         createButton.setOnClickListener(view ->{
@@ -210,6 +231,230 @@ public class CreateRideActivity extends AppCompatActivity  implements OnMapReady
             }
         });
 
+
+        inputFrom.setOnItemClickListener(autocompleteClickListener);
+        adapter = new AutoCompleteAdapter(this, placesClient);
+        inputFrom.setAdapter(adapter);
+
+        inputTo.setOnItemClickListener(autocompleteClickListenerTo);
+        adapter1 = new AutoCompleteAdapter(this, placesClient);
+        inputTo.setAdapter(adapter1);
+
+
+    }
+
+    private AdapterView.OnItemClickListener autocompleteClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            try {
+                final AutocompletePrediction item = adapter.getItem(i);
+                String placeID = null;
+                if (item != null) {
+                    placeID = item.getPlaceId();
+                }
+
+//                To specify which data types to return, pass an array of Place.Fields in your FetchPlaceRequest
+//                Use only those fields which are required.
+
+                List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS
+                        , Place.Field.LAT_LNG);
+
+                FetchPlaceRequest request = null;
+                if (placeID != null) {
+                    request = FetchPlaceRequest.builder(placeID, placeFields)
+                            .build();
+                }
+
+                if (request != null) {
+                    placesClient.fetchPlace(request).addOnSuccessListener(new OnSuccessListener<FetchPlaceResponse>() {
+                        @SuppressLint({"SetTextI18n", "MissingPermission"})
+                        @Override
+                        public void onSuccess(FetchPlaceResponse task) {
+                            Log.e(TAG,"from click");
+
+                            inputTo.requestFocus();
+
+                            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                                return;
+                            }
+                            googleMap.setMyLocationEnabled(true);
+                            // For zooming automatically to the location of the marker
+
+                            if (googleMap != null) {
+
+                                googleMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                                    @Override
+                                    public void onMyLocationChange(Location arg0) {
+
+//                                        if(fromClick) {
+
+
+                                        googleMap.addMarker(new MarkerOptions().position(task.getPlace().getLatLng()).title(task.getPlace().getName()));
+                                        CameraPosition cameraPosition = new CameraPosition.Builder().target(task.getPlace().getLatLng()).zoom(12).build();
+                                        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+                                        fName = task.getPlace().getName();
+                                        fAddress = task.getPlace().getAddress();
+                                        fPlaceID = task.getPlace().getId();
+                                        fLat = task.getPlace().getLatLng().latitude;
+                                        fLong = task.getPlace().getLatLng().longitude;
+                                        inputFrom.setText(fName);
+
+//                                        }
+
+//                                        if(toClick){
+//
+//                                            googleMap.addMarker(new MarkerOptions().position(task.getPlace().getLatLng()).title(task.getPlace().getName()));
+//                                            CameraPosition cameraPosition1 = new CameraPosition.Builder().target(task.getPlace().getLatLng()).zoom(12).build();
+//                                            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition1));
+//
+//
+//                                            tName = task.getPlace().getName();
+//                                            tAddress = task.getPlace().getAddress();
+//                                            tPlaceID = task.getPlace().getId();
+//                                            tLat = task.getPlace().getLatLng().latitude;
+//                                            tLong = task.getPlace().getLatLng().longitude;
+//                                            inputTo.setText(tName);
+//
+//                                            start=new LatLng(fLat,fLong);
+//                                            end=new LatLng(tLat,tLong);
+//
+//                                            Findroutes(start,end);
+//                                            toClick=false;
+//                                        }
+
+
+                                    }
+                                });
+                            }
+                            // The user canceled the operation.
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            e.printStackTrace();
+//                            responseView.setText(e.getMessage());
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    };
+    private AdapterView.OnItemClickListener autocompleteClickListenerTo = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            try {
+                final AutocompletePrediction item = adapter1.getItem(i);
+                String placeID = null;
+                if (item != null) {
+                    placeID = item.getPlaceId();
+                }
+
+//                To specify which data types to return, pass an array of Place.Fields in your FetchPlaceRequest
+//                Use only those fields which are required.
+
+                List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS
+                        , Place.Field.LAT_LNG);
+
+                FetchPlaceRequest request = null;
+                if (placeID != null) {
+                    request = FetchPlaceRequest.builder(placeID, placeFields)
+                            .build();
+                }
+
+                if (request != null) {
+
+                    placesClient.fetchPlace(request).addOnSuccessListener(new OnSuccessListener<FetchPlaceResponse>() {
+                        @SuppressLint({"SetTextI18n", "MissingPermission"})
+                        @Override
+                        public void onSuccess(FetchPlaceResponse task) {
+
+                            inputTo.clearFocus();// MainActivity is the name of the class and v is the View parameter used in the button listener method onClick.
+
+                            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                                return;
+                            }
+                            googleMap.setMyLocationEnabled(true);
+                            // For zooming automatically to the location of the marker
+
+                            if (googleMap != null) {
+
+                                googleMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                                    @Override
+                                    public void onMyLocationChange(Location arg0) {
+
+//
+//                                            googleMap.addMarker(new MarkerOptions().position(task.getPlace().getLatLng()).title(task.getPlace().getName()));
+//                                            CameraPosition cameraPosition1 = new CameraPosition.Builder().target(task.getPlace().getLatLng()).zoom(12).build();
+//                                            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition1));
+
+
+                                        tName = task.getPlace().getName();
+                                        tAddress = task.getPlace().getAddress();
+                                        tPlaceID = task.getPlace().getId();
+                                        tLat = task.getPlace().getLatLng().latitude;
+                                        tLong = task.getPlace().getLatLng().longitude;
+                                        inputTo.setText(tName);
+
+                                        start=new LatLng(fLat,fLong);
+                                        end=new LatLng(tLat,tLong);
+
+                                        Findroutes(start,end);
+//
+
+////
+                                    }
+                                });
+                            }
+                            // The user canceled the operation.
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            e.printStackTrace();
+//                            responseView.setText(e.getMessage());
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    };
+
+    // function to find Routes.
+    public void Findroutes(LatLng Start, LatLng End)
+
+    {
+
+        Log.e(TAG,"start "+ Start+ "end "+ End);
+        if(Start==null || End==null) {
+            Toast.makeText(CreateRideActivity.this,"Unable to get location", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+
+            Log.v(TAG," st" + Start+" end"+ end);
+
+            Routing routing = new Routing.Builder()
+                    .travelMode(AbstractRouting.TravelMode.DRIVING)
+                    .withListener(this)
+                    .alternativeRoutes(true)
+                    .waypoints(Start, End)
+                    .key(getString(R.string.google_geo_api_key))
+                    .build();
+            routing.execute();
+        }
     }
 
     private void initialize() {
@@ -394,6 +639,99 @@ public class CreateRideActivity extends AppCompatActivity  implements OnMapReady
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+
+    //Routing call back functions.
+    @Override
+    public void onRoutingFailure(RouteException e) {
+        View parentLayout = findViewById(android.R.id.content);
+        Snackbar snackbar= Snackbar.make(parentLayout, e.toString(), Snackbar.LENGTH_LONG);
+        snackbar.show();
+    }
+
+    @Override
+    public void onRoutingStart() {
+//
+//        View parentLayout = findViewById(android.R.id.content);
+//        Snackbar snackbar= Snackbar.make(parentLayout, "Finding Route...", Snackbar.LENGTH_LONG);
+//        snackbar.show();
+    }
+
+    //If Route finding success..
+    @Override
+    public void onRoutingSuccess(ArrayList<Route> route, int shortestRouteIndex) {
+
+        Log.e(TAG,"route succes");
+
+
+
+        if(polylines!=null) {
+            polylines.clear();
+        }
+        PolylineOptions polyOptions = new PolylineOptions();
+        LatLng polylineStartLatLng=null;
+        LatLng polylineEndLatLng=null;
+
+
+        polylines = new ArrayList<>();
+        //add route(s) to the map using polyline
+        for (int i = 0; i <route.size(); i++) {
+
+            if(i==shortestRouteIndex)
+            {
+                polyOptions.color(getResources().getColor(R.color.brown));
+                polyOptions.width(12);
+                polyOptions.addAll(route.get(shortestRouteIndex).getPoints());
+
+                Polyline polyline = googleMap.addPolyline(polyOptions);
+                polylineStartLatLng=polyline.getPoints().get(0);
+                int k=polyline.getPoints().size();
+                polylineEndLatLng=polyline.getPoints().get(k-1);
+                polylines.add(polyline);
+
+
+
+            }
+            else {
+
+            }
+
+        }
+
+        //Add Marker on route starting position
+        MarkerOptions startMarker = new MarkerOptions();
+        startMarker.position(polylineStartLatLng);
+        startMarker.title("My Location");
+        googleMap.addMarker(startMarker);
+
+        //Add Marker on route ending position
+        MarkerOptions endMarker = new MarkerOptions();
+        endMarker.position(polylineEndLatLng);
+        endMarker.title("Destination");
+        googleMap.addMarker(endMarker);
+
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        builder.include(polylineStartLatLng);
+        builder.include(polylineEndLatLng);
+        LatLngBounds bounds = builder.build();
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 100);
+        googleMap.animateCamera(cu);
+
+    }
+
+
+    @Override
+    public void onRoutingCancelled() {
+        Log.e(TAG,"cancelled");
+        Findroutes(start,end);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(TAG,"failed");
+
+        Findroutes(start,end);
 
     }
 }
