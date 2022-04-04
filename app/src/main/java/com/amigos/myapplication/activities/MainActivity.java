86552757ee2ba2dd793439d488b13f0551ea0efd
Simranjit Singh <C0809865@mylambton.ca>
2022-04-04 12:42:50 -0400

    Added App Rating Prompt

--- app/src/main/java/com/amigos/myapplication/activities/MainActivity.java
+++ app/src/main/java/com/amigos/myapplication/activities/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import com.amigos.myapplication.R;
@@ -15,6 +16,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import hotchemi.android.rate.AppRate;
+import hotchemi.android.rate.OnClickButtonListener;
 import im.crisp.client.Crisp;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +28,26 @@ public class MainActivity extends AppCompatActivity {
 
         //Initializing Crisp Chat Box
         Crisp.configure(getApplicationContext(), "566b6f31-c4f5-4b3d-a7c8-61e2c4776f97");
+
+
+        //Google App Rating Dialog Box
+
+        AppRate.with(this)
+                .setInstallDays(0) // default 10, 0 means install day.
+                .setLaunchTimes(5) // default 10
+                .setRemindInterval(2) // default 1
+                .setShowLaterButton(true) // default true
+                .setDebug(false) // default false
+                .setOnClickButtonListener(new OnClickButtonListener() { // callback listener.
+                    @Override
+                    public void onClickButton(int which) {
+                        Log.d(MainActivity.class.getName(), Integer.toString(which));
+                    }
+                })
+                .monitor();
+
+        // Show a dialog if meets conditions
+        AppRate.showRateDialogIfMeetsConditions(this);
     }
 
     @Override
